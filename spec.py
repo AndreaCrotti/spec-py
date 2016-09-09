aaa4fc0bd1f522f5ce37c342279247fc7ad5e780
Andrea Crotti <andrea.crotti.0@gmail.com>
2016-09-09 14:26:50 +0100

    add first ideas

--- spec.py
+++ spec.py
@@ -1,17 +1,39 @@
 import toolz
 
 
+# TODO: need a generic function to walk over the dictionary smartly enough
+
+def transform(inp, filter, func):
+    pass
+
+
 def select(dic, path):
     sub_dic = dic.copy()
+    is_last = lambda idx: idx == len(path) - 1
+
     for idx, p in enumerate(path):
         if p in dic:
-            sub_dic = dic[p]
-            
+            if is_last(idx):
+                yield sub_dic[p]
+            else:
+                sub_dic = sub_dic[p]
 
-def test_long_name():
+
+def test_select_simple_case():
     inp = {
         "a": {"b": 1},
         "b": {"b": 1},
     }
     
     assert list(select(inp, ['a', 'b'])) == [1]
+
+
+def test_transform():
+    inp = {
+        1: {"b": 1},
+        2: {"b": 1},
+    }
+
+    is_even = lambda k: k % 2 == 0
+    inc = lambda n: n + 1
+    assert transform(inp, filter=is_even, func=inc) == {1: {"b": 1}, 2: {"b": 2}}
