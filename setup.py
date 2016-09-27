5bda8ebe849a89174619a4da98bb369ded7601cf
Andrea Crotti <andrea.crotti.0@gmail.com>
2016-09-27 16:20:03 +0100

    add some more structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+package = 'spec-py'
+version = '0.1'
+
+INSTALL_REQUIRES = ['toolz>=0.8']
+
+setup(name=package,
+      version=version,
+      description="Simple specification",
+      url='')
--- spec.py
+++ spec.py
@@ -2,9 +2,23 @@ import toolz
 
 
 # TODO: need a generic function to walk over the dictionary smartly enough
+# TODO: should work both on dictionaries and on list of dictionaries
 
 def transform(inp, filter, func):
     pass
+
+
+def traverse(inp, func):
+    new_result = {}
+    # TODO: check how the traverse function in Clojure works
+    if not isinstance(inp, dict):
+        return func(inp)
+
+    else:
+        for key, val in inp.items():
+            new_result[key] = traverse(val, func)
+
+    return new_result
 
 
 def select(dic, path):
@@ -37,3 +51,9 @@ def test_transform():
     is_even = lambda k: k % 2 == 0
     inc = lambda n: n + 1
     assert transform(inp, filter=is_even, func=inc) == {1: {"b": 1}, 2: {"b": 2}}
+
+
+def test_traverse():
+    func = lambda x: x
+    # TODO: test it with more mixed types
+    assert traverse({1: 2}, func) == {1: 2}
