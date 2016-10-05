4843a46ba74ce4bf8249f21d80e9f941e0e966b0
Andrea Crotti <andrea.crotti.0@gmail.com>
2016-10-05 17:25:35 +0100

    classifiers and license

--- setup.py
+++ setup.py
@@ -7,6 +7,20 @@ INSTALL_REQUIRES = ['toolz>=0.8']
 
 setup(name=package,
       version=version,
-      packages=find_packages(),
+      packages=['spec'],
       description="Simple specification",
-      url='')
+      license='MIT',
+      author="Andrea Crotti",
+      author_email="andrea.crotti.0@gmail.com",
+      classifiers=[
+          "Development Status :: 4 - Beta",
+          "License :: OSI Approved :: MIT License",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.3",
+          "Programming Language :: Python :: 3.4",
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: Implementation :: CPython",
+          "Programming Language :: Python :: Implementation :: PyPy"],
+      url='https://github.com/AndreaCrotti/depr')
