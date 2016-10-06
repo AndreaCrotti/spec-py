045f5247309e97639ec8e676ca353e4d59d7ed4b
Andrea Crotti <andrea.crotti.0@gmail.com>
2016-10-06 14:05:25 +0100

    fix setup

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
-package = 'spec-py'
+package = 'spec'
 version = '0.1'
 
-INSTALL_REQUIRES = ['toolz>=0.8']
+INSTALL_REQUIRES = ['toolz>=0.8', 'voluptuous>=0.8.7']
 
 setup(name=package,
       version=version,
@@ -12,8 +12,9 @@ setup(name=package,
       license='MIT',
       author="Andrea Crotti",
       author_email="andrea.crotti.0@gmail.com",
+      install_requires=INSTALL_REQUIRES,
       classifiers=[
-          "Development Status :: 4 - Beta",
+          "Development Status :: 3 - ALpha",
           "License :: OSI Approved :: MIT License",
           "Programming Language :: Python",
           "Programming Language :: Python :: 2.7",
@@ -23,4 +24,4 @@ setup(name=package,
           "Programming Language :: Python :: 3.5",
           "Programming Language :: Python :: Implementation :: CPython",
           "Programming Language :: Python :: Implementation :: PyPy"],
-      url='https://github.com/AndreaCrotti/depr')
+      url='https://github.com/AndreaCrotti/spec')
